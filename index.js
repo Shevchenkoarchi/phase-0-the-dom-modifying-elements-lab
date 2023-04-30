e109187f3f6f6579169c120a1ca3bf1c0b406f41
arturshevchenko <shevchenkoarchi@gmail.com>
2023-04-30 11:11:52 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+const mainRemove = document.querySelector("#main");
+mainRemove.remove();
+const newHeader = document.createElement("h1");
+newHeader.id = 'victory'
+newHeader.innerHTML = "YOUR-NAME is the champion"
